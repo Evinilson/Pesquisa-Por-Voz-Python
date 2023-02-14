09d7e0ab73a9828514cbe554bc77cb1663c30ac4
Evinilson Fernandes <evinilsonsilva16@gmail.com>
2023-02-14 14:42:38 +0000

    Update voice_search.py
    crie a função, reconhecer fala e main

--- voice_search.py
+++ voice_search.py
@@ -1,21 +1,29 @@
 import speech_recognition as sr
 
 # Cria um objeto de reconhecimento de fala
-r = sr.Recognizer()
+microfone = sr.Recognizer()
 
-# Usa o microfone como fonte de áudio
-with sr.Microphone() as source:
-    print("Fale algo...")
-    # Ajusta o nível de ruído de fundo do microfone para uma melhor transcrição
-    r.adjust_for_ambient_noise(source)
-    # Captura o áudio
-    audio = r.listen(source)
+def reconhecer_fala():
+    try:
+        # Usa o microfone como fonte de áudio
+        with sr.Microphone() as source:
+            print("Fale algo...")
+            # Ajusta o nível de ruído de fundo do microfone para uma melhor transcrição
+            microfone.adjust_for_ambient_noise(source)
+            # Captura o áudio
+            audio = microfone.listen(source)
+        # Transcreve o áudio em texto usando o Google Speech Recognition
+        texto = microfone.recognize_google(audio, language='pt-PT')
+        return texto
+    except sr.UnknownValueError:
+        print("Não foi possível transcrever o áudio")
+    except sr.RequestError as e:
+        print("Erro ao conectar-se ao serviço de reconhecimento de fala: {0}".format(e))
 
-# Transcreve o áudio em texto usando o Google Speech Recognition
-try:
-    text = r.recognize_google(audio, language='pt-BR')
-    print("Você disse: ", text)
-except sr.UnknownValueError:
-    print("Não foi possível transcrever o áudio")
-except sr.RequestError as e:
-    print("Erro ao conectar-se ao serviço de reconhecimento de fala: {0}".format(e))
+def main():
+    print("Este é um programa de pesquisa por voz. Por favor, fale o que deseja pesquisar.")
+    texto_transcrito = reconhecer_fala()
+    if texto_transcrito is not None:
+        print(f"Você disse: {texto_transcrito}")
+
+main()
